56e64029dc51a164d79a0d461dc4650650b7711f
jasenfranklin <70617412+jasenfranklin@users.noreply.github.com>
2023-12-06 19:12:02 -0700

    Add files via upload

--- BST.java
+++ BST.java
@@ -1,4 +1,5 @@
 import java.lang.Math;
+
 public class BST<K extends Comparable<K>, V> {
 	public static int nodeCount = 0;// do not get rid of this!
 	Node root;
@@ -21,23 +22,18 @@ public class BST<K extends Comparable<K>, V> {
 			root = newNode;
 			root.height = 0;
 			return;
-		} Node tleft = root.left();
-		  Node tright = root.right();
-			root = putHelp(root, newNode);
-		if (tleft == null&&tright!=null) {
-			root.height = tright.height + 1;
-		} else if (tright == null && tleft!=null) {
-			root.height = tleft.height + 1;
 		}
+		root = putHelp(root, newNode);
 	}
 
 	private Node putHelp(Node rt, Node comp) {
-        Node tempR = rt.right;
-        Node tempL = rt.left;
+		Node tempR = rt.right();
+		Node tempL = rt.left();
 		if (comp.key.compareTo(rt.key) > 0) {
 			if (tempR == null) {
 				comp.height = 0;
 				rt.right(comp);
+				tempR = comp;
 			} else {
 				putHelp(tempR, comp);
 			}
@@ -45,13 +41,26 @@ public class BST<K extends Comparable<K>, V> {
 			if (tempL == null) {
 				comp.height = 0;
 				rt.left(comp);
+				tempL = comp;
 			} else {
 				putHelp(tempL, comp);
 			}
 		} else if (rt.key.compareTo(comp.key) == 0) {
 			rt.val = comp.val;
 		}
-		rt.height=Math.max(rt.left.height, rt.right.height)+1;
+		if (tempR != null && tempL != null) {
+			rt.height = Math.max(tempL.height, tempR.height) + 1;
+			rt.N++;
+			return rt;
+		} else if (tempL != null) {
+			rt.height = tempL.height + 1;
+			rt.N++;
+			return rt;
+		} else if (tempR != null) {
+			rt.height = tempR.height + 1;
+			rt.N++;
+			return rt;
+		}
 		return rt;
 	}
 
@@ -62,26 +71,27 @@ public class BST<K extends Comparable<K>, V> {
 		if (root.key == null) {
 			return null;
 		}
-		if (key.compareTo(root.key) > 0) {
-			return getHelp(root.right(), key).val;
+		Node retVal = getHelp(root, key);
+		if (retVal == null) {
+			return null;
 		}
-		if (key.compareTo(root.key) < 0) {
-			return getHelp(root.left(), key).val;
-		}
-		return root.val;
+		return retVal.val;
 	}
 
 	/***
 	 * assists get with returning a key
 	 */
 	private Node getHelp(Node rt, K key) {
+		if (rt == null) {
+			return null;
+		}
 		if (key.compareTo(rt.key) == 0) {
 			return rt;
 		}
 		if (key.compareTo(rt.key) > 0) {
-			getHelp(rt.right(), key);
+			return getHelp(rt.right(), key);
 		} else if (key.compareTo(rt.key) < 0) {
-			getHelp(rt.left(), key);
+			return getHelp(rt.left(), key);
 		}
 		return null;
 	}
@@ -102,7 +112,7 @@ public class BST<K extends Comparable<K>, V> {
 	 * return true if the tree is empty and false otherwise
 	 */
 	public boolean isEmpty() {
-		if (nodeCount == 0) {
+		if (root == null) {
 			return true;
 		}
 		return false;
@@ -112,7 +122,10 @@ public class BST<K extends Comparable<K>, V> {
 	 * return the number of Nodes in the tree
 	 */
 	public int size() {
-		return nodeCount;
+		if (root == null) {
+			return 0;
+		}
+		return root.N;
 	}
 
 	/***
@@ -143,37 +156,57 @@ public class BST<K extends Comparable<K>, V> {
 	 * key does not exist in the tree
 	 ***/
 	public int depth(K key) {
-        int depthcount = 0;
-        Node compaNode = new Node(key, null);
-		return depthHelp(compaNode, depthcount);
-		
+		int depthcount = 0;
+		return depthHelp(root, key, depthcount);
+
 	}
 
-    private int depthHelp(Node key, int depthCount){        
-        if(root.key!=null&&root.key.compareTo(key.key)>0){
-            depthHelp(root.right, depthCount+1);
-        }if(root.key!=null&&root.key.compareTo(key.key)<0){
-            depthHelp(root.left, depthCount+1);
-        }if(root.key!=null&&root.key.compareTo(key.key)==0){
-            return depthCount;
-        }
-        return -1;
-    }
+	private int depthHelp(Node rt, K key, int depthCount) {
+		if (rt == null) {
+			return -1;
+		}
+		if (key.compareTo(rt.key) == 0) {
+			return depthCount;
+		}
+		if (key.compareTo(rt.key) > 0) {
+			return depthHelp(rt.right(), key, (depthCount + 1));
+		} else if (key.compareTo(rt.key) < 0) {
+			return depthHelp(rt.left(), key, (depthCount + 1));
+		}
+		return -1;
+	}
 
 	/***
 	 * return the size of the subtree rooted at the node with the given key return
 	 * -1 if the key is not in the tree
 	 ***/
 	public int size(K key) {
-		// to be implemented
-		return 0;
+		Node temp = getHelp(root, key);
+		if (temp != null) {
+			return temp.N;
+		}
+		return -1;
 	}
 
 	/***
 	 * return the minimum key or null if the tree is empty
 	 ***/
 	public K min() {
-		// to be implemented
+		if (root == null) {
+			return null;
+		} else {
+			return minHelp(root);
+		}
+	}
+
+	private K minHelp(Node rt) {
+		Node temp = rt.left();
+		if (temp != null) {
+			return minHelp(temp);
+		}
+		if (temp == null) {
+			return rt.key;
+		}
 		return null;
 	}
 
@@ -181,8 +214,19 @@ public class BST<K extends Comparable<K>, V> {
 	 * return the maximum key or nul if the tree is empty
 	 ***/
 	public K max() {
-		return null;
-		// to be implemented
+		if (root == null) {
+			return null;
+		} else {
+			return maxHelp(root);
+		}
+	}
+
+	private K maxHelp(Node rt) {
+		Node temp = rt.right();
+		if (temp != null) {
+			return maxHelp(temp);
+		}
+		return rt.key;
 	}
 
 	/***
@@ -190,8 +234,30 @@ public class BST<K extends Comparable<K>, V> {
 	 * such a key does not exist
 	 ***/
 	public K floor(K key) {
-		// to be implemented
+		Node temp = floorHelp(root, key);
+		if (temp != null) {
+			return temp.key;
+		}
 		return null;
+	}
+
+	private Node floorHelp(Node rt, K key) {
+		if (rt.key == key) {
+			return rt;
+		}
+		if (key.compareTo(rt.key) > 0) {
+			Node compR = rt.right();
+			if (compR != null && key.compareTo(compR.key) >= 0) {
+				return floorHelp(compR, key);
+			} else {
+				return rt;
+			}
+		}
+		Node compL = rt.left();
+		if (compL != null && key.compareTo(rt.key) < 0) {
+			return floorHelp(compL, key);
+		}
+		return rt;
 	}
 
 	/***
@@ -199,8 +265,40 @@ public class BST<K extends Comparable<K>, V> {
 	 * null if such a key does not exist
 	 ***/
 	public K ceil(K key) {
-		// to be implemented
-		return null;
+		Node temp = ceilHelp(root, key);
+		if (temp != null) {
+			return temp.key;
+		}
+		return root.key;
+	}
+
+	private Node ceilHelp(Node rt, K key) {
+		if (rt.key == key) {
+			return rt;
+		} else if (rt == null) {
+			return rt;
+		}
+		if (key.compareTo(rt.key) < 0) {
+			Node tempL = rt.left();
+			if (tempL != null) {
+				if(key.compareTo(tempL.key)>0&&tempL.right()==null) {
+					return rt;
+				}
+				return ceilHelp(tempL, key);
+			}
+		}
+		if (key.compareTo(rt.key) > 0) {
+			Node tempR = rt.right();
+			if (tempR == null) {
+				return rt;
+			}if(key.compareTo(tempR.key)<0) {
+				if(tempR.left()!=null) {
+					ceilHelp(tempR.left(), key);
+				}
+			}
+			return ceilHelp(tempR, key);
+		}
+		return rt;
 	}
 
 	/***
@@ -208,8 +306,52 @@ public class BST<K extends Comparable<K>, V> {
 	 * does not exist
 	 ***/
 	public int rank(K key) {
-		// to be implemented
-		return -999;
+		int retVal = rankHelp(root, key, 0);
+		return retVal;
+	}
+
+	private int rankHelp(Node rt, K key, int compVal) {
+		if (rt == null) {
+			return -1;
+		}
+
+		Node tempL = rt.left();
+		if (key.compareTo(rt.key) == 0) {
+			if (tempL == null) {
+				return 0;
+			} else {
+				return tempL.N;
+			}
+		}
+
+		if (key.compareTo(rt.key) > 0) {
+			Node tempR = rt.right();
+			if (tempR != null && tempL != null) {
+				int sum1 = tempL.N + 1;
+				int sum2 = rankHelp(tempR, key, compVal);
+				if (sum2 == -1) {
+					return -1;
+				}
+				return sum1 + sum2;
+			}else if(tempR!=null&&tempL==null) {
+				int sum1=1;
+				int sum2=rankHelp(tempR, key, compVal);
+				if(sum2==-1) {
+					return -1;
+				}
+				return sum1+sum2;
+			}else {
+				return -1;
+			}
+		} else if (key.compareTo(rt.key) < 0) {
+			int sum3 = rankHelp(tempL, key, compVal);
+			if (sum3 == -1) {
+				return -1;
+			} else {
+				return sum3;
+			}
+		}
+		return -1;
 	}
 
 	/***
@@ -217,16 +359,40 @@ public class BST<K extends Comparable<K>, V> {
 	 * sense given the tree
 	 ***/
 	public K select(int rank) {
-		// to be implemented
-		return null;
+
+		return root.key;
 	}
 
 	/***
 	 * return the number of keys in [lo...hi]
 	 ***/
 	public int size(K lo, K hi) {
-		// to be implemented
-		return -1;
+		int temp = sizeHelp(lo, hi, root);
+		System.out.println(temp);
+		return temp;
+	}
+
+	private int sizeHelp(K lo, K hi, Node rt) {
+		if (rt == null) {
+			return 0;
+		}
+		// if lo is grerater / == than root-----if hi is less= than root
+		if (lo.compareTo(rt.key) <= 0 && hi.compareTo(rt.key) >= 0) {
+			Node tempR = rt.right();
+			Node tempL = rt.left();
+			int leftSide = sizeHelp(lo, hi, tempR);
+			int rightSide = sizeHelp(lo, hi, tempL);
+			return leftSide + rightSide + 1;
+			// if hi is less than / eql to root
+		} else if (lo.compareTo(rt.key) >= 0) {
+			Node tempR = rt.right();
+			return sizeHelp(lo, hi, tempR);
+		} else if (hi.compareTo(rt.key) <= 0) {
+			Node tempL = rt.left();
+			return sizeHelp(lo, hi, tempL);
+		} else {
+			return 0;
+		}
 	}
 
 	/* NODE CLASS */
@@ -261,9 +427,10 @@ public class BST<K extends Comparable<K>, V> {
 		public void left(Node l) {
 			nodeCount++;
 			this.left = l;
+			;
 		}
 
-		// getter for the right child
+		// setter for the right child
 		public void right(Node r) {
 			nodeCount++;
 			this.right = r;
--- RBT.java
+++ RBT.java
@@ -2,159 +2,299 @@ public class RBT<K extends Comparable<K>, V> {
     private static final boolean RED = true;
     private static final boolean BLACK = false;
     private Node root;
-    private int n;
-
     /* CONSTRUCTOR */
     public RBT() {
-	//to be implemented
+        root = null;
     }
 
     /* PUBLIC METHODS */
 
     /***
-     *insert a new (key, val) into tree
-     *or replace value of existing key
+     * insert a new (key, val) into tree
+     * or replace value of existing key
      */
     public void put(K key, V val) {
-	//to be implemented
+        Node newNode = new Node(key, val);
+        if (root == null) {
+            newNode.color = BLACK;
+            root = newNode;
+        } else {
+            newNode.color = RED;
+            root = putHelp(root, newNode);
+
+        }
     }
-    
+
+    private Node putHelp(Node rt, Node comp) {
+        if (comp.key.compareTo(rt.key) == 0) {
+            rt.val = comp.val;
+        } else if (comp.key.compareTo(rt.key) < 0) {
+            if (rt.left == null) {
+                comp.height = 0;
+                rt.left = comp;
+            } else {
+                rt.left = putHelp(rt.left, comp);
+            }
+
+        } else if (comp.key.compareTo(rt.key) > 0) {
+            if (rt.right == null) {
+                comp.height = 0;
+                rt.right = comp;
+            } else {
+                rt.right = putHelp(rt.right, comp);
+            }
+        }
+        if (rt.right != null && rt.left != null) {
+            rt.height = Math.max(rt.left.height, rt.right.height) + 1;
+            rt.N++;
+            return rt;
+        } else if (rt.left != null) {
+            rt.height = rt.left.height + 1;
+            rt.N++;
+            return rt;
+        } else if (rt.right != null) {
+            rt.height = rt.right.height + 1;
+            rt.N++;
+            return rt;
+        }
+
+        if (rt.left != null && rt.left.color) {
+            rt = rotateRight(rt);
+            fixHeight(rt);
+        }
+        if (rt.right != null && rt.right.color && rt.right.right != null && rt.right.right.color) {
+            rt = rotateLeft(rt);
+            fixHeight(rt);
+        }
+        if (rt.left != null && rt.left.color && rt.right != null && rt.right.color) {
+            colorFlip(rt);
+        }
+        return rt;
+    }
+
+    private int fixHeight(Node rt) {
+        int leftH = 0;
+        int rightH = 0;
+        if (rt.left != null) {
+            leftH = fixHeight(rt.left);
+        }
+        if (rt.right != null) {
+            rightH = fixHeight(rt.right);
+        }
+        if (rt.right == null && rt.left == null) {
+            return Math.max(leftH, rightH);
+        }
+
+        else {
+            return (1 + Math.max(leftH, rightH));
+        }
+    }
+
     /***
-     *get the value associated with the given key;
-     *return null if key doesn't exist
+     * get the value associated with the given key;
+     * return null if key doesn't exist
      */
     public V get(K key) {
-	//to be implemented
+        Node temp = getHelp(root, key);
+        if (isEmpty()) {
+            return null;
+        }
+        if(temp== null){
+            return null;
+        }
+        return temp.val;
+    }
+    //helps the get by cycling down
+    private Node getHelp(Node rt, K key) {
+        if (key.compareTo(rt.key) == 0) {
+            return root;
+        }
+        if (key.compareTo(rt.key) < 0) {
+            if (rt.left == null) {
+                return null;
+            } else {
+                return getHelp(rt.left, key);
+            }
+        }
+        if (key.compareTo(rt.key) > 0) {
+            if (rt.right == null) {
+                return null;
+            } else {
+                return getHelp(rt.right, key);
+            }
+        }
+        return null;
     }
 
     /***
-     *get the color of a node;
-     *return "RED" if it is red,
-     *"BLACK" if it is black,
-     *and null if it does not exist
+     * get the color of a node;
+     * return "RED" if it is red,
+     * "BLACK" if it is black,
+     * and null if it does not exist
      ***/
     public String getColor(K key) {
-	//to be implemented
-	return null;
+        Node temp = getHelp(root, key);
+        if (temp != null) {
+            if (temp.color) {
+                return "RED";
+            } else {
+                return "Black";
+            }
+        }
+        return null;
     }
 
     /***
-     *return true if the tree
-     *is empty and false 
-     *otherwise
+     * return true if the tree
+     * is empty and false
+     * otherwise
      */
     public boolean isEmpty() {
-	//to be implemented
+        return root == null;
     }
 
     /***
-     *return the number of Nodes
-     *in the tree
+     * return the number of Nodes
+     * in the tree
      */
     public int size() {
-	//to be implemented
+        if (root == null) {
+            return 0;
+        }
+        return root.N;
     }
 
     /***
-     *returns the height of the tree
+     * returns the height of the tree
      */
     public int height() {
-	//to be implemented
+        return root.height;
     }
 
     /***
-     *returns the height of node 
-     *with given key in the tree;
-     *return -1 if the key does
-     *not exist in the tree
+     * returns the height of node
+     * with given key in the tree;
+     * return -1 if the key does
+     * not exist in the tree
      */
     public int height(K key) {
-	//to be implemented
+        if (isEmpty()) {
+            return -1;
+        }
+        Node temp = getHelp(root, key);
+        if(temp==null){
+            return -1;
+        }
+        return temp.height;
     }
 
-     /***
-     *return the depth of the node
-     *with the given key in the tree;
-     *return -1 if the key does not exist
-     *in the tree
+    /***
+     * return the depth of the node
+     * with the given key in the tree;
+     * return -1 if the key does not exist
+     * in the tree
      ***/
     public int depth(K key) {
-	//to be implemented
+        int depthcount = 0;
+        return depthHelp(root, key, depthcount);
+
+    }
+
+    private int depthHelp(Node rt, K key, int depthCount) {
+        if (rt == null) {
+            return -1;
+        }
+        if (key.compareTo(rt.key) == 0) {
+            return depthCount;
+        }
+        if (key.compareTo(rt.key) > 0) {
+            return depthHelp(rt.right, key, (depthCount + 1));
+        } else if (key.compareTo(rt.key) < 0) {
+            return depthHelp(rt.left, key, (depthCount + 1));
+        }
+        return -1;
     }
 
     /***
-     *return true if the key exists in the tree
-     *and false otherwise
+     * return true if the key exists in the tree
+     * and false otherwise
      ***/
     public boolean contains(K key) {
-	//to be implemented
+        if (getHelp(root, key) != null) {
+            return true;
+        }
+        return false;
     }
 
     /***
-     *return the number of nodes in the subtree
-     *that is rooted at the node with the given key
-     *or -1 if the key does not exist
+     * return the number of nodes in the subtree
+     * that is rooted at the node with the given key
+     * or -1 if the key does not exist
      ***/
     public int size(K key) {
-	//to be implemented
+        Node temp = getHelp(root, key);
+        if (temp != null) {
+            return temp.N;
+        }
+        return -1;
     }
 
     /* PRIVATE METHODS */
 
     /***
-     *performs the rotate left operation
+     * performs the rotate left operation
      ***/
     private Node rotateLeft(Node h) {
-	Node x = h.right;
-	h.right = x.left;
-	x.left = h;
-	x.color = h.color;
-	h.color = RED;
-	return x;
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
     }
 
     /***
-     *performs the rotate right operation
+     * performs the rotate right operation
      ***/
     private Node rotateRight(Node h) {
-	Node x = h.left;
-	h.left = x.right;
-	x.right = h;
-	x.color = h.color;
-	h.color = RED;
-	return x;
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
     }
 
     /***
-     *performs the color flip operation
+     * performs the color flip operation
      ***/
     private void colorFlip(Node h) {
-	h.color = RED;
-	h.left.color = BLACK;
-	h.right.color = BLACK;
-	if(h == root)
-	    h.color = BLACK;
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+        if (h == root)
+            h.color = BLACK;
     }
 
     /* NODE CLASS */
-    /***WARNING: DO NOT CHANGE THIS CLASS!***/
+    /*** WARNING: DO NOT CHANGE THIS CLASS! ***/
     public class Node {
-	K key;
-	V val;
-	Node left, right;
-	int height;
-	int N;
-	boolean color;
+        K key;
+        V val;
+        Node left, right;
+        int height;
+        int N;
+        boolean color;
 
-	public Node(K key, V val) {
-	    this.key = key;
-	    this.val = val;
-	    this.N = 1;
-	    this.color = RED;
-	}
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+            this.N = 1;
+            this.color = RED;
+        }
 
-	public String toString() {
-	    return "(" + key + ", " + val + ")";
-	}
+        public String toString() {
+            return "(" + key + ", " + val + ")";
+        }
     }
-}    
+}
